7d712c4b5329e0ec661791da999227cf54979e0b
savan <savannahwilliams243@gmail.com>
2023-12-20 14:40:14 -0800

    created new tag with .create

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,6 +52,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
