47414aca63c458f61a6cfe8d1454160e31618268
savan <savannahwilliams243@gmail.com>
2023-12-20 14:31:15 -0800

    created new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,6 +52,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
